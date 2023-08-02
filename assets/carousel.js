2b246cb550406beb44ac593bfc2b28d31dadfb66
Valene-R <valene.renaud@gmail.com>
2023-08-02 14:07:10 +0200

    refactor(carousel): make carousel JS more readable

--- assets/carousel.js
+++ assets/carousel.js
@@ -1,34 +1,93 @@
-// Récupére toutes les images de la galerie
+/////
+// CONSTANTES / VARIABLES
+/////
+
+// Récupére les images de la galerie
 const imagesGallery = document.querySelectorAll(".gallery-item");
+const allTags = getAllTags(imagesGallery);
 
-// Stocke tous les tags disponibles
-const allTags = new Set(); // Utilise Set pour éviter les doublons de tags
-imagesGallery.forEach(image => {
-    // Récupére le tag directement depuis l'attribut "data-gallery-tag"
-    const tag = image.dataset.galleryTag;
-    // Ajoute le tag à "allTags"
-    allTags.add(tag);
-});
+// Récupére les éléments de la modale
+const modal = document.querySelector('.modal');
+const modalImg = document.querySelector('.modal-content');
 
-// Récupére les boutons de filtre de la galerie
-const filterButtonsContainer = document.querySelector('.filter-buttons-container');
+// Récupére les boutons précédent et suivant pour naviguer dans le carrousel
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
 
-// Crée un bouton "Tous" pour afficher toutes les images
-const allButton = document.createElement('button');
-allButton.textContent = 'Tous';
-allButton.dataset.filter = 'all';
-filterButtonsContainer.appendChild(allButton);
+let currentIndex = 0;
+let currentFilter = 'all'; // Par défaut, affiche toutes les images
 
-// Crée des boutons pour chaque tag
-allTags.forEach(tag => {
-    const button = document.createElement('button');
-    button.textContent = tag;
-    button.dataset.filter = tag;
-    filterButtonsContainer.appendChild(button);
-});
 
-// Fonction pour sélectionner le tag et ajouter le fond vert aux boutons
+/////
+// FONCTIONS
+/////
+
+/**
+ * Stockage de tous les tags disponibles dans un Set
+ * @param {NodeList} imagesGallery - La liste des images de la galerie
+ * @returns {Set} - Un Set contenant tous les tags
+ */ 
+function getAllTags(imagesGallery) {
+    const tagsSet = new Set(); // Utilise Set pour éviter les doublons de tags
+    imagesGallery.forEach(image => {
+        // Récupére le tag directement depuis l'attribut "data-gallery-tag"
+        const tag = image.dataset.galleryTag;
+        // Ajoute le tag à "tagsSet"
+        tagsSet.add(tag); 
+    });
+    return tagsSet;
+}
+
+/**
+ * Création des boutons de filtre de la galerie
+ * @param {Set} allTags - Un Set contenant tous les tags 
+ */
+function createFilterButtons(allTags) {
+    // Sélectionne le conteneur des boutons de filtre
+    const filterButtonsContainer = document.querySelector('.filter-buttons-container');
+    
+    // Crée un bouton "Tous" pour afficher toutes les images
+    const allButton = document.createElement('button');
+    allButton.textContent = 'Tous';
+    allButton.dataset.filter = 'all';
+    filterButtonsContainer.appendChild(allButton);
+
+    // Crée un bouton pour chaque tag
+    allTags.forEach(tag => {
+        const button = document.createElement('button');
+        button.textContent = tag;
+        button.dataset.filter = tag;
+        filterButtonsContainer.appendChild(button);
+    });
+
+    // Associe les boutons de filtre aux événements de clic
+    const filterButtons = document.querySelectorAll('.filter-buttons-container button');
+    filterButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            // Sélectionne le tag et met à jour l'apparence des boutons
+            selectTag(button);
+
+            const filter = button.dataset.filter;
+            // Filtre les images en fonction du tag sélectionné
+            filterImages(filter);
+
+            // Mise à jour du carrousel en fonction du tag sélectionné
+            updateCarousel(filter);
+        });
+    });
+
+    // Sélectionne le bouton "Tous" par défaut au chargement de la page
+    const defaultSelectButton = document.querySelector('.filter-buttons-container button[data-filter="all"]');
+    selectTag(defaultSelectButton);
+}
+createFilterButtons(allTags);
+
+/**
+ * Sélection du tag et ajout du fond vert jaunâtre aux boutons
+ * @param {HTMLElement} button - Le bouton du filtre sélectionné 
+ */
 function selectTag(button) {
+    const filterButtons = document.querySelectorAll('.filter-buttons-container button');
     filterButtons.forEach(btn => {
         if (btn === button) {
             btn.classList.add('selectBtnTag');
@@ -38,59 +97,47 @@ function selectTag(button) {
     });
 }
 
-// Sélectionne tous les boutons de filtre
-const filterButtons = document.querySelectorAll('.filter-buttons-container button');
-filterButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        selectTag(button);
-
-        // Récupére le filtre sélectionné
-        const filter = button.dataset.filter;
-
-        // Filtre les images en fonction du filtre sélectionné
-        if (filter === 'all') {
-            imagesGallery.forEach(image => image.style.display = 'block');
+/**
+ * Filtrage des images en fonction du tag sélectionné
+ * @param {string} filter - Le tag du filtre sélectionné 
+ */
+function filterImages(filter) {
+    const imagesGallery = document.querySelectorAll(".gallery-item");
+    imagesGallery.forEach(image => {
+        const tag = image.dataset.galleryTag;
+        if (filter === 'all' || tag.includes(filter)) {
+            image.style.display = 'block';
         } else {
-            imagesGallery.forEach(image => {
-                const tag = image.dataset.galleryTag;
-                if (tag.includes(filter)) {
-                    image.style.display = 'block';
-                } else {
-                    image.style.display = 'none';
-                }
-            });
+            image.style.display = 'none';
         }
-
-        // Affiche la galerie avec une gille de 3 colonnes
-        const gallery = document.querySelector('.gallery');
-        gallery.style.display = 'grid';
-        gallery.style.gridTemplateColumns = 'repeat(3, 1fr)';
-        gallery.style.gridGap = '10px';
-
-        // Mise à jour du carrousel en fonction du tag sélectionné
-        updateCarousel(filter);
     });
-});
 
-// Fonction pour mettre à jour le filtre et le carrousel en fonction du tag sélectionné
-let currentFilter = 'all'; // Par défaut, affiche toutes les images
+    // Affiche la galerie avec une grille de 3 colonnes
+    const gallery = document.querySelector('.gallery');
+    gallery.style.display = 'grid';
+    gallery.style.gridTemplateColumns = 'repeat(3, 1fr)';
+    gallery.style.gridGap = '10px';
+}
+
+/**
+ * Mise à jour du filtre actuel et du carrousel en fonction du tag sélectionné
+ * @param {string} filter - Le tag du filtre sélectionné
+ */
 function updateCarousel(filter) {
     currentFilter = filter;
     showImage(currentIndex);
 }
 
-// Sélectionne le bouton "Tous" par défaut au chargement de la page
-const defaultSelectButton = document.querySelector('.filter-buttons-container button[data-filter="all"]');
-selectTag(defaultSelectButton);
-
-// Récupére les éléments de la modale
-const modal = document.querySelector('.modal');
-const modalImg = document.querySelector('.modal-content');
-const darkBackground = document.getElementById('dark-background');
-
-// Fonction pour ouvrir la modale avec l'image correspondant à l'index 
+/**
+ * Ouverture de la modale
+ * @param {number} index - Index de l'image en cours
+ */
 function openModal(index) {
+    const modal = document.querySelector('.modal');
+    const darkBackground = document.getElementById('dark-background');
+
     modal.style.display = 'block';
+    // Affiche l'arrière-plan sombre
     darkBackground.style.display = 'block';
     currentIndex = index;
     showImage(currentIndex);
@@ -98,14 +145,19 @@ function openModal(index) {
     // Attendre un court délai pour activer l'animation de descente en ajoutant la classe .show
     setTimeout(() => {
         modal.classList.add('show');
-    }, 50); // Durée de l'animation de descente (0.05s)
+    }, 50);// Durée de l'animation de descente (0.05s)
 
     // Empêche le défilement du body lorsque la modale est ouverte
     document.body.style.overflow = 'hidden';
 }
 
-// fonction pour fermer la modale
+/**
+ * Fermeture de la modale
+ */
 function closeModal() {
+    const modal = document.querySelector('.modal');
+    const darkBackground = document.getElementById('dark-background');
+
     // Supprime la classe .show pour activer l'animation de montée
     modal.classList.remove('show');
 
@@ -117,32 +169,13 @@ function closeModal() {
 
         // Rétablit le défilement du body lorsque la modale est fermée
         document.body.style.overflow = 'auto';
-
-    }, 300); // Durée de l'animation de montée (0.3s)
+    }, 300);// Durée de l'animation de montée (0.3s)
 }
 
-// Lorsque l'une des images de la galerie est cliquée, ouvre la modale en fonction de l'index de l'image
-imagesGallery.forEach((image, index) => {
-    image.addEventListener('click', () => {
-        openModal(index);
-    });
-});
-
-// Lorsque la modale est cliquée
-modal.addEventListener('click', (event) => {
-    // Si l'élément cliqué est la modale (et non son contenu) en comparant avec l'événement actuel
-    if (event.target === modal) {
-        // Appelle la fonction closeModal pour la fermer
-        closeModal();
-    }
-});
-
-// Récupére les boutons précédent et suivant pour naviguer dans le carrousel
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-let currentIndex = 0;
-
-// Fonction pour afficher l'image correspondant à l'index 
+/**
+ * Affichage de l'image correspondant à l'index
+ * @param {number} index - Index de l'image à afficher
+ */
 function showImage(index) {
     const image = imagesGallery[index];
     const tag = image.dataset.galleryTag;
@@ -153,6 +186,27 @@ function showImage(index) {
         currentIndex = index;
     }
 }
+
+
+/////
+// EVENT LISTENERS
+/////
+
+// Écouteur du clic pour les images de la galerie
+imagesGallery.forEach((image, index) => {
+    image.addEventListener('click', () => {
+        openModal(index);
+    });
+});
+
+// Écouteur du clic pour la modale
+modal.addEventListener('click', (event) => {
+    // Si l'élément cliqué est la modale (et non son contenu) en comparant avec l'événement actuel
+    if (event.target === modal) {
+        // Appelle la fonction closeModal pour la fermer
+        closeModal();
+    }
+});
 
 // Écouteur du clic pour le bouton précédent
 prevBtn.addEventListener('click', () => {
@@ -183,3 +237,4 @@ nextBtn.addEventListener('click', () => {
         nextIndex = (nextIndex + 1) % imagesGallery.length;
     }
 });
+
